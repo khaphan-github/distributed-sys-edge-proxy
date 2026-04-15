3a8b3bd4ad51db52fc760780a6b314049d4df705
phkha <phkha>
2026-04-15 14:21:43 +0700

    Change service type to ClusterIP for app-b and app-c; remove redundant build script and integrate builds into setup script

--- cluster/setup_cluster.sh
+++ cluster/setup_cluster.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 docker build -t edge_proxy_app-a:v1.0.1 ./app-a
+docker build -t edge_proxy_app-b:v1.0.1 ./app-b
+docker build -t edge_proxy_app-c:v1.0.1 ./app-c
+docker build -t edge_proxy_envoy:v1.0.1 ./envoy
 
 minikube image load edge_proxy_app-a:v1.0.1
 minikube image load edge_proxy_app-b:v1.0.1
