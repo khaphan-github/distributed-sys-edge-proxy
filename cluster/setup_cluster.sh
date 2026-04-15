689995e9236410ae8ac8812a170d56f186552e74
phkha <phkha>
2026-04-15 14:01:42 +0700

    Refactor Kubernetes deployment files for apps A, B, and C; consolidate Envoy configurations into single deployment files and update setup script for streamlined deployment.

--- cluster/setup_cluster.sh
+++ cluster/setup_cluster.sh
@@ -8,18 +8,9 @@ minikube image load edge_proxy_envoy:latest
 minikube image ls
 
 # Deploy service
-kubectl apply -f app-a/k8s/deployment-svc.yaml
-kubectl apply -f app-a/k8s/configmap-envoy.yaml
-kubectl apply -f app-a/k8s/deployment-envoy-app.yaml
-
-
-kubectl apply -f app-b/k8s/deployment-svc.yaml
-kubectl apply -f app-b/k8s/configmap-envoy.yaml
-kubectl apply -f app-b/k8s/deployment-envoy-app.yaml
-
-kubectl apply -f app-c/k8s/deployment-svc.yaml
-kubectl apply -f app-c/k8s/configmap-envoy.yaml
-kubectl apply -f app-c/k8s/deployment-envoy-app.yaml
+kubectl apply -f app-a/k8s/deployment.yaml
+kubectl apply -f app-b/k8s/deployment.yaml
+kubectl apply -f app-c/k8s/deployment.yaml
 
 kubectl get svc
 #
