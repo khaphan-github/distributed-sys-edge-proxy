6b03dedee69043f316c54fec79f4364b8362c0ee
khaphan-github <phanhoangkha01@gmail.com>
2026-04-17 02:24:46 +0700

    Update Docker images for app-b and app-c to version 1.0.1; change Envoy image to latest in setup script

--- cluster/setup_cluster.sh
+++ cluster/setup_cluster.sh
@@ -2,12 +2,12 @@
 docker build -t edge_proxy_app-a:v1.0.1 ./app-a
 docker build -t edge_proxy_app-b:v1.0.1 ./app-b
 docker build -t edge_proxy_app-c:v1.0.1 ./app-c
-docker build -t edge_proxy_envoy:v1.0.1 ./envoy
+docker build -t edge_proxy_envoy:latest ./envoy
 
 minikube image load edge_proxy_app-a:v1.0.1
 minikube image load edge_proxy_app-b:v1.0.1
 minikube image load edge_proxy_app-c:v1.0.1
-minikube image load edge_proxy_envoy:v1.0.1
+minikube image load edge_proxy_envoy:latest
 
 minikube image ls
 
